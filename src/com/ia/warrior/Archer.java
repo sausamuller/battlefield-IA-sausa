ccf522a16ee2d5b25a02d80b3e8da86afb455c99
DESKTOP-Flor <flordvt95@gmail.com>
2017-08-21 01:41:24 -0300

    - Funciona sourcetree con eclipse

--- src/com/ia/warrior/Archer.java
+++ src/com/ia/warrior/Archer.java
@@ -6,7 +6,7 @@ import ia.battle.core.actions.Action;
 import ia.exceptions.RuleException;
 
 public class Archer extends Warrior {
-//flor
+//flor hhhhh
 	public Archer(String name, int health, int defense, int strength, int speed, int range) throws RuleException {
 		super(name, health, defense, strength, speed, range);
 		// TODO Auto-generated constructor stub
