1cda64646e2fe56c3b8946db299c3d98db35bacf
lsausa <lautarosausa@gmail.com>
2017-08-20 21:15:14 -0300

    Creation of clases for the battlefield.

---
+++ src/com/ia/manager/Ruler.java
@@ -0,0 +1,21 @@
+package com.ia.manager;
+
+import ia.battle.core.Warrior;
+import ia.battle.core.WarriorManager;
+import ia.exceptions.RuleException;
+
+public class Ruler extends WarriorManager {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Warrior getNextWarrior() throws RuleException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
