612ff327216e7b818599e46de7e79d7b3df3a162
facum <facumillivieri@gmail.com>
2017-08-21 02:02:04 -0300

    Elimino Comentario de flor

--- src/com/ia/warrior/Archer.java
+++ src/com/ia/warrior/Archer.java
@@ -6,8 +6,6 @@ import ia.battle.core.actions.Action;
 import ia.exceptions.RuleException;
 
 public class Archer extends Warrior {
-//flor fjhsdafjh
-
 	public Archer(String name, int health, int defense, int strength, int speed, int range) throws RuleException {
 		super(name, health, defense, strength, speed, range);
 		// TODO Auto-generated constructor stub
