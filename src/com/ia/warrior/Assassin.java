a0c8dbc64ff18deb025e1b6c8b4695d4d3e1ce5a
lsausa <lsausa@sysone>
2017-09-15 22:56:47 -0300

    Creation of assassin strategy. Creation of strategy classes.

--- src/com/ia/warrior/Assassin.java
+++ src/com/ia/warrior/Assassin.java
@@ -1,21 +1,27 @@
 package com.ia.warrior;
 
+import com.ia.strategy.AssassinStrategy;
+import com.ia.strategy.Strategy;
+
+import ia.battle.core.BattleField;
 import ia.battle.core.FieldCell;
 import ia.battle.core.Warrior;
 import ia.battle.core.actions.Action;
 import ia.exceptions.RuleException;
 
 public class Assassin extends Warrior {
+	
+	private Strategy strategy = new AssassinStrategy();
 
 	public Assassin(String name, int health, int defense, int strength, int speed, int range) throws RuleException {
 		super(name, health, defense, strength, speed, range);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public Action playTurn(long tick, int actionNumber) {
-		// TODO Auto-generated method stub
-		return null;
+		strategy.setPosition(getPosition());
+		strategy.setEnemyPosition(BattleField.getInstance().getEnemyData().getFieldCell());
+		return strategy.playTurn(tick, actionNumber);
 	}
 
 	@Override
